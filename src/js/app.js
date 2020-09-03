cfcbf9150eb2d3697c964ba43e70270a6b3e59f6
kobancheg <aleks.revati@gmail.com>
2020-09-03 15:28:24 +0300

    show notifications

--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,7 @@ import UI from './config/ui.config';
 import { validate } from './helpers/validate';
 import { showInputError, removeInputError } from './views/form';
 import { login } from './services/auth.service';
+import { notify } from './views/notification';
 
 const { form, inputEmail, inputPassword } = UI;
 const inputs = [inputEmail, inputPassword];
@@ -31,7 +32,10 @@ async function onSubmit() {
   try {
     await login(inputEmail.value, inputPassword.value);
     form.reset();
+    // show success notify
+    notify({ msg: 'Login success', className: 'alert-success' })
   } catch (err) {
-
+    // show alert notify
+    notify({ msg: 'Login faild', className: 'alert-danger' })
   }
 }
