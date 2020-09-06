39dd7a6a0a8b68264949e0ff5b83de67bfef1aaa
kobancheg <aleks.revati@gmail.com>
2020-09-06 19:36:26 +0300

    added registration forms & sent credentials user

--- src/js/app.js
+++ src/js/app.js
@@ -7,19 +7,25 @@ import { showInputError, removeInputError } from './views/form';
 import { login } from './services/auth.service';
 import { notify } from './views/notification';
 import { getNews } from './services/news.servise';
+import { signUp } from './services/signup.service';
 
-const { form, inputEmail, inputPassword } = UI;
+const { form, inputEmail, inputPassword, signUpForm } = UI;
 const inputs = [inputEmail, inputPassword];
 
 // Events
 form.addEventListener('submit', e => {
   e.preventDefault();
-  onSubmit();
+  onSubmitLogin();
 });
 inputs.forEach(el => el.addEventListener('focus', () => removeInputError(el)));
 
+signUpForm.addEventListener('submit', e => {
+  e.preventDefault();
+  onSubmitSignUp();
+});
+
 // Handlers
-async function onSubmit() {
+async function onSubmitLogin() {
   const isValidForm = inputs.every(el => {
     const isValidInput = validate(el);
     if (!isValidInput) {
@@ -39,5 +45,46 @@ async function onSubmit() {
   } catch (err) {
     // show alert notify
     notify({ msg: 'Login faild', className: 'alert-danger' })
+  }
+}
+
+async function onSubmitSignUp() {
+  // const isValidForm = inputs.every(el => {
+  //   const isValidInput = validate(el);
+  //   if (!isValidInput) {
+  //     showInputError(el);
+  //   }
+  //   return isValidInput;
+  // });
+
+  // if (!isValidForm) return;
+
+  let date = UI.birthday.value.split('-')
+
+  const signUpValue = {
+    email: UI.signUpEmail.value,
+    password: UI.signUpPassword.value,
+    nickname: UI.nickName.value,
+    first_name: UI.firstName.value,
+    last_name: UI.lastName.value,
+    phone: UI.phone.value,
+    gender_orientation: UI.gender.value,
+    city: UI.city.value,
+    country: UI.country.value,
+    date_of_birth_day: +date[2],
+    date_of_birth_month: +date[1],
+    date_of_birth_year: +date[0]
+  }
+
+  console.log(signUpValue)
+
+  try {
+    await signUp(signUpValue);
+    form.reset();
+    // show success notify
+    // notify({ msg: 'Login success', className: 'alert-success' })
+  } catch (err) {
+    // show alert notify
+    // notify({ msg: 'Login faild', className: 'alert-danger' })
   }
 }
--- src/js/config/ui.config.js
+++ src/js/config/ui.config.js
@@ -2,6 +2,18 @@ const UI = {
   form: document.forms['loginForm'],
   inputEmail: document.getElementById('email'),
   inputPassword: document.getElementById('password'),
+
+  signUpForm: document.forms['signUpForm'],
+  signUpEmail: document.getElementById('signUpEmail'),
+  signUpPassword: document.getElementById('signUpPassword'),
+  nickName: document.getElementById('nickname'),
+  firstName: document.getElementById('firstname'),
+  lastName: document.getElementById('lastname'),
+  phone: document.getElementById('phone'),
+  gender: document.getElementById('gender'),
+  country: document.getElementById('country'),
+  city: document.getElementById('city'),
+  birthday: document.querySelector('input[type="date"]'),
 };
 
 export default UI;
